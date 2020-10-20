38c8b0f167dabede013a96cf7ead89bf3c2a7adc
no-debugger <73200485+no-debugger@users.noreply.github.com>
2020-10-21 00:03:56 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+var minimalUserResponseInMiliseconds = 200;
+function check() {
+    before = new Date().getTime();
+    debugger ;after = new Date().getTime();
+    if (after - before > minimalUserResponseInMiliseconds) {
+        alert("You think you're clever?\nDo not open Developer Tools.");
+        self.location.replace(window.location.protocol + window.location.href.substring(window.location.protocol.length));
+    } else {
+        before = null;
+        after = null;
+        delete before;
+        delete after;
+    }
+    setTimeout(check, 100);
+}
+check();
