91e0dc0b6214055328f2487f015b46257be4e00c
no-debugger <73200485+no-debugger@users.noreply.github.com>
2020-11-14 16:47:08 +0100

    Create instant.js

---
+++ instant.js
@@ -0,0 +1,17 @@
+/*
+* Stable version 4.0 - Instant No Debugger
+* A program to disable and prevent downloads in the developer console instantly.
+* 2020 John Cailtyn
+*/
+
+let dk = document.createElement('div');
+let lo = setInterval(() => {
+	console.log(dk);
+	console.clear();
+});
+Object.defineProperty(dk, "id", {
+	get: () => {
+		clearInterval(lo);
+		window.location = window.location.hostname+"/ban.html?url="+window.location;
+	}
+});
